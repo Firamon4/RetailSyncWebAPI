08852b6bcaed1b6032f775e92897b17d140e53ab
s.treskin <s.treskin@TRESKIN-S>
2026-01-26 10:01:00 +0200

    1.3

--- RetailSyncWeb/Program.cs
+++ RetailSyncWeb/Program.cs
@@ -1,34 +1,34 @@
 using Microsoft.EntityFrameworkCore;
 using RetailSyncWeb.Data;
 using RetailSyncWeb.Services;
-using RetailSyncWeb.Components; // Для Blazor
+using RetailSyncWeb.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. ПІДКЛЮЧЕННЯ БАЗИ 
 var connectionString = "Server=localhost\\SQLEXPRESS;Database=RetailSyncDB;Trusted_Connection=True;TrustServerCertificate=True;";
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
-// 2. СЕРВІСИ
-builder.Services.AddControllers(); // Для API
-builder.Services.AddRazorComponents().AddInteractiveServerComponents(); // Для Blazor UI
+// ВАЖЛИВО: Замість AddDbContext використовуємо AddDbContextFactory
+builder.Services.AddDbContextFactory<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); // Для тестування API
+builder.Services.AddSwaggerGen();
 
-// Реєструємо наш фоновий процесор
-builder.Services.AddSingleton<SyncStatusService>(); 
+builder.Services.AddSingleton<SyncStatusService>();
 builder.Services.AddHostedService<PackageProcessor>();
 
 var app = builder.Build();
 
-// 3. ІНІЦІАЛІЗАЦІЯ БАЗИ (Авто-створення таблиць)
+// Ініціалізація бази через фабрику
 using (var scope = app.Services.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<AppDbContext>>();
+    using var db = factory.CreateDbContext();
     db.Database.EnsureCreated();
 }
 
-// 4. НАЛАШТУВАННЯ HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
@@ -37,14 +37,10 @@ if (!app.Environment.IsDevelopment())
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
-
-// Маршрути (включаємо і UI, і API)
 app.MapControllers();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
 app.Run();
